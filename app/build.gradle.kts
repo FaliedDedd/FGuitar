782f2a89b61ba61ede5bd234564a8d9d9dcb9901
Mr Falied <149243666+FaliedDedd@users.noreply.github.com>
2025-05-26 22:42:42 +0300

    ota-3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "org.thefproject.fguitar"
         minSdk = 24
         targetSdk = 35
-        versionCode = 1
-        versionName = "0.1"
+        versionCode = 21
+        versionName = "0.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
